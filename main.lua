a7555551c77160188ce9c152b88d3cec87b7bca4
Tate Larsen <larset2@gmail.com>
2011-10-09 17:55:47 -0400

    Level Seclect Menu

--- main.lua
+++ main.lua
@@ -12,9 +12,11 @@ audio.setVolume(0.0)
 function mainMenu()
     mainMenuGroup = display.newGroup()
     
+    -- Load Background Image
     local mainMenuBG = display.newImage("mainMenuBG.png")
     mainMenuGroup:insert(mainMenuBG)
 
+    -- Add buttons
     local newGameButton = display.newImage("newGameButton.png", (display.contentWidth / 2) - 48, (display.contentHeight / 2) + 60)
     newGameButton.id = newGame
     mainMenuGroup:insert(newGameButton)
@@ -23,43 +25,46 @@ function mainMenu()
     levelSelectButton.id = levelSelect
     mainMenuGroup:insert(levelSelectButton)
     
+    -- Add the sound option
     local soundButtonSheet = sprite.newSpriteSheet("SoundIcon.png", 30, 30)
     local soundButtonSet = sprite.newSpriteSet(soundButtonSheet, 1, 2)
     sprite.add(soundButtonSet, "off", 2, 1, 1000)
     sprite.add(soundButtonSet, "on", 1, 1, 1000)
-    local soundButton = sprite.newSprite(soundButtonSet)
+    soundButton = sprite.newSprite(soundButtonSet)
     mainMenuGroup:insert(soundButton)
     soundButton.x = display.contentWidth - 50
     soundButton.y = 50
     soundButton:prepare("off")
     soundButton:play()
     
-    -- Main Menu Sound
-    BGSound = audio.loadSound("sound_of_storms.mp3")
+    -- Main Menu Background Music
+    BGSound = audio.loadSound("song_of_storms.mp3")
     BGChannel = audio.play(BGSound, {loops = -1})
     
     newGameButton:addEventListener("touch", init)
     levelSelectButton:addEventListener("touch", init)
-    soundButton:addEventListener("touch", function(event)
-                                            if event.phase == "began" then
-                                                if soundState == 2 then
-                                                    soundState = 1
-                                                    audio.setVolume(0.0)
-                                                    soundButton:prepare("off")
-                                                    soundButton:play()
-                                                else
-                                                    soundState = 2
-                                                    audio.setVolume(1.0)
-                                                    soundButton:prepare("on")
-                                                    soundButton:play()
-                                                end
-                                            end
-                                          end)
+    soundButton:addEventListener("touch", toggleSound)
     
 end
 
 function levelSelectMenu()
     levelSelectGroup = display.newGroup()
+    
+    -- Load Background Image
+    local levelSelectBG = display.newImage("levelSelectBG.png")
+    levelSelectGroup:insert(levelSelectBG)
+    
+    
+    -- Add level selection buttons
+    local levelButtons = {}
+    
+    for i=1,4 do
+        levelButtons[i] = display.newImage(("level" .. i .. "Button.png"), (((i - 1) % 5 ) + 1) * 120, 100 + ((math.floor(i / 5)) * 70))
+        levelButtons[i].id = i
+        levelSelectGroup:insert(levelButtons[i])
+        levelButtons[i]:addEventLisenter("touch", loadLevel)
+    end
+    
 end
 
 function inGameMenu()
@@ -79,6 +84,25 @@ function init(event)
         --local levelSelectAnimation = transition.to(mainMenuGroup, {alpha = 0, xScale = 1, yScale = 1, time = 400})
         levelSelectMenu()
     end
+end
+
+function toggleSound(event)
+    if event.phase == "began" then
+        if soundState == 2 then
+            soundState = 1
+            audio.setVolume(0.0)
+            soundButton:prepare("off")
+            soundButton:play()
+        else
+            soundState = 2
+            audio.setVolume(1.0)
+            soundButton:prepare("on")
+            soundButton:play()
+        end
+    end
+end
+
+function loadLevel(event)
 end
 
 mainMenu()
